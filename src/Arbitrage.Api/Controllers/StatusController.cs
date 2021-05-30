981a53faeb76c1aa5a5bcd5664eb0b5dd1956692
Zoyo23 <lucasoliveiraoueiroz@gmail.com>
2021-05-30 09:46:03 -0300

    Alteração da rota padrão para Statuscontroller

---
+++ src/Arbitrage.Api/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Arbitrage.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+
+            RecurringJob.AddOrUpdate("Teste", () => RunBackground(), "*/5 * * * * *");
+
+            return Ok("Funfando! :P");
+        }
+
+        public void RunBackground()
+        {
+            Console.WriteLine($"$ Teste de Background Task...");
+        }
+    }
+}
